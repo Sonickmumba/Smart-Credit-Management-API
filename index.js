1ae997f35687ef0b6973c4e893e840243ffa96d9
Sonick Mumba <kingellie.mumba@gmail.com>
2024-08-31 02:49:44 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require("dotenv").config();
+
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+  res.json({
+    info: "Node.js, Express, and Postgres API Template by Sonick",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+})
