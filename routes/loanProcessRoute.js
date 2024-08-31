0d591e37c58f701738f047cd20f5eec8aabdab46
Sonick Mumba <kingellie.mumba@gmail.com>
2024-08-31 02:55:54 +0200

    Create loanProcessRoute.js

---
+++ routes/loanProcessRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const loanController = require('../controllers/loanController');
+
+const router = express.Router();
+
+
+router.post('/process', loanController);
