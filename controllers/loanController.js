63982643be098527cb4b76d08ea8854fc321767b
Sonick Mumba <kingellie.mumba@gmail.com>
2024-09-04 14:58:20 +0200

    Update loanController.js

--- controllers/loanController.js
+++ controllers/loanController.js
@@ -4,8 +4,10 @@ const processLoanApplication = async (req, res) => {
   const { borrowerId, loanAmount } = req.body;
 
   // Input validation
-  if (!borrowerId || borrowerId === '' || !loanAmount || loanAmount <= 0) {
-    return res.status(400).json({ success: false, message: "Invalid or Missing input" });
+  if (!borrowerId || borrowerId === "" || !loanAmount || loanAmount <= 0) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid or Missing input" });
   }
 
   // if (!borrowerId || borrowerId === "") {
@@ -38,10 +40,65 @@ const processLoanApplication = async (req, res) => {
       creditLimitData = borrowerResults;
     }
 
-    
+    const remainingCreditLimit =
+      results.rows[0].credit_limit - results.rows[0].used_amount;
 
+    // Check for unpaid loans
+    const unpaidLoans = await pool.query(
+      "SELECT * FROM loan WHERE borrower_id = $1 AND repayment_date < NOW() AND payment_status = 'Not Paid'",
+      [borrowerId]
+    );
 
+    if (unpaidLoans.rows.length > 0) {
+      return res.status(400).json({
+        Success: false,
+        message:
+          "There are existing unpaid loans with crossed repayment dates."
+      });
+    }
 
+    if (loanAmount > remainingCreditLimit) {
+      return res.status(400).json({
+        Success: false,
+        message: "Loan amount exceeds the remaining credit limit."
+      });
+    }
+
+    const loanDate = new Date();
+    const repaymentDate = new Date(loanDate);
+    repaymentDate.setMonth(repaymentDate.getMonth() + 1);
+    const paymentStatus = "Not Paid";
+    const paidDate = null;
+
+    const loanInsert = await pool.query(
+      "INSERT INTO loan (borrower_id, loan_amount, loan_date, repayment_date, payment_status, paid_date) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [borrowerId, loanAmount, loanDate, repaymentDate, paymentStatus, paidDate]
+    );
+    // console.log(loanInsert.rows);
+    await pool.query(
+      "UPDATE credit_limit SET used_amount = used_amount + $1, remaining_amount = credit_limit - used_amount - $1 WHERE borrower_id = $2 RETURNING *",
+      [loanAmount, borrowerId]
+    );
+
+    const loanId = loanInsert.rows[0].id;
+    const penaltyAmount = 0.0;
+    const totalAmount = penaltyAmount + loanAmount;
+    const paidOnDate = null;
+
+    await pool.query(
+      "INSERT INTO payment_transaction (loan_id, loan_amount, penalty_amount, total_amount, repayment_date, paid_on_date, payment_status) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [
+        loanId,
+        loanAmount,
+        penaltyAmount,
+        totalAmount,
+        repaymentDate,
+        paidOnDate,
+        paymentStatus,
+      ]
+    );
+
+    return res.status(200).json(loanInsert.rows[0]);
   } catch (error) {
     res.status(500).json({
       message: "An error occurred while processing the loan application",
