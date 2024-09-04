a6e2338ad795d14f1fa6a176296b0e14411ed13a
Sonick Mumba <kingellie.mumba@gmail.com>
2024-09-04 16:29:09 +0200

    Update loanProcessRoute.js

--- routes/loanProcessRoute.js
+++ routes/loanProcessRoute.js
@@ -5,8 +5,9 @@ const loanController = require('../controllers/loanController');
 const router = express.Router();
 
 router.get('/', loanController.viewAllLoans);
-
+router.get('/:loanId', loanController.viewLoanById);
 router.post('/process', loanController.processLoanApplication);
+
 
 
 module.exports = router;
