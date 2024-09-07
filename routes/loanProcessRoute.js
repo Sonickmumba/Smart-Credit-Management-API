8d924990e90d17c31e36a5241490456ab71d146e
Sonick Mumba <kingellie.mumba@gmail.com>
2024-09-07 23:18:03 +0200

    Update loanProcessRoute.js

--- routes/loanProcessRoute.js
+++ routes/loanProcessRoute.js
@@ -5,7 +5,9 @@ const loanController = require('../controllers/loanController');
 const router = express.Router();
 
 router.get('/', loanController.viewAllLoans);
+
 router.get('/:loanId', loanController.viewLoanById);
+
 router.get('/borrowers/:borrowerId', loanController.viewLoansByBorrowerId);
 router.get('/creditlimit/:borrowerId', loanController.viewCreditLimitByBorrowerId);
 router.get('/paymentdetails/:loanId', loanController.getPaymentDetails);
